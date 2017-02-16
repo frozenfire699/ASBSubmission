0f12f42967a62ba7c88b7cc76952e8fdcff7abfc
Frozenfire <Frozenfire@MEGRO.local>
2017-02-16 13:20:07 -0700

    Creation of page to Submit Entry , and Results page to show final results - Using localStorage.

---
+++ js/data.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+
+myApp.service('dataService', function() {
+
+    this.teams = ['ATL', 'DEN'];
+
+});
--- js/routing.js
+++ js/routing.js
@@ -1,5 +1,5 @@
 // Registering my angular App
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngStorage']);
 
 // Creating the routing configuration
 myApp.config(function ($routeProvider) {
@@ -21,9 +21,47 @@ $routeProvider
 
 
 // Creating the Entry Page Controller
-myApp.controller('entryController', function($scope, $log) {
+myApp.controller('entryController', function($scope, $log,
+                                            $localStorage ) {
 
-    $scope.name = 'Entry';
+    //$scope.teamName = 'Empty';
+    //$scope.rscs = '';
+    //$scope.skillsChal = '';
+    //$scope.threeChal = '';
+    //$scope.slamChal = '' ;
+    //$scope.asgs = '';
+    //$scope.tieBreak = '';
+
+    $scope.entryObj = {
+        teamName :'' ,
+        rscs : '',
+        skillsChal : '',
+        threeChal : '',
+        slamChal : '',
+        asgs : '',
+        tieBreak : ''
+    }
+ $scope.save = function() {
+
+     $log.info($scope.entryObj);
+        window.localStorage.setItem("entryObj", JSON.stringify($scope.entryObj));
+
+    }
+
+    //$scope.teams = dataService.teams;
+    $scope.teams = ["ATL","WAS", "ORL", "MIA", "CHA",
+                    "MIN", "DEN", "OKC", "UTA", "POR",
+                    "BKN", "BOS", "TOR", "NYK", "PHI",
+                    "DET", "CLE", "CHI", "IND", "MIL",
+                    "MEM", "HOU", "SAS", "DAL", "NOP",
+                   "LAL", "LAC", "GSW", "PHO", "SAC",];
+    $scope.skillsPlayers = ["Joel Embidd" , "Anthony Davis",
+                            "Kristaps Prozingis", "Nikola Jokic", "Demarcus Cousins", "Gordon Hayward", "Devin Booker", "John Wall", "Isiah Thomas"];
+    $scope.threePlayers = ["Klay Thompson" , "CJ McCollum",
+                            "Kyle Lowry", "Eric Gordon", "Kyrie Irving", "Kemba Walker", "Nick Young", "Wesley Matthews"];
+    $scope.slamPlayers = ["Glen Robinson III" , "DeAndre Jordon",
+                            "Aaron Gordon", "Derrick Jones Jr"]
+
 
 });
 
@@ -31,5 +69,12 @@ myApp.controller('entryController', function($scope, $log) {
 myApp.controller('resultsController', function($scope, $log) {
 
     $scope.name = 'Results';
+
+     $scope.load = function(){
+
+     $scope.entryObj = JSON.parse(window.localStorage.getItem("entryObj"));
+
+     $log.info($scope.entryObj);
+ }
 
 });
